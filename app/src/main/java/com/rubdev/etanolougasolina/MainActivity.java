a9a2df032becf92c611633652747db257ebea1d1
Rubens <rubao.ma@gmail.com>
2020-07-28 11:05:23 -0300

    adicionado logica de calculo

--- app/src/main/java/com/rubdev/etanolougasolina/MainActivity.java
+++ app/src/main/java/com/rubdev/etanolougasolina/MainActivity.java
@@ -3,12 +3,67 @@ package com.rubdev.etanolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoEtanol;
+    private TextInputEditText editPrecoGasolina;
+    private TextView textResultado;
+    private Button calcular;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoEtanol = findViewById(R.id.editPrecoEtanol);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
+
     }
+
+
+    public void calcularPreco(View view) {
+        // recuperar valores digitados
+        String precoEtanol = editPrecoEtanol.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+        Boolean camposValidados = validarCampos(precoEtanol, precoGasolina);
+
+        if (camposValidados){
+
+            Double valorEtanol = Double.parseDouble( precoEtanol);
+            Double valorGasolina = Double.parseDouble( precoGasolina);
+
+            if ( valorEtanol / valorGasolina >= 0.7){
+                textResultado.setText("Melhor utilizar Gasolina");
+            } else {
+                textResultado.setText("Melhor utilizar Etanol");
+            }
+
+        }else {
+            textResultado.setText("Preencha os preços primeiro!");
+        }
+
+
+    }
+
+    public Boolean validarCampos(String pEtanol, String pGasolina){
+        Boolean camposValidados = true;
+
+        if (pEtanol == null || pEtanol.equals("")){
+            camposValidados = false;
+        }else if (pGasolina == null || pGasolina.equals("")){
+            camposValidados = false;
+        }
+
+        return camposValidados;
+    }
+
+
 }
